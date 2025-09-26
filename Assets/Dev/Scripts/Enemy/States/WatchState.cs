4977527ea33cf1db0753e59867aca9dde2c6f39f
Ezequiel Guerra <ezequielguerra005@gmail,com>
2025-09-26 12:38:59 -0300

    Modificaciones a WatchState + Barra de detección del enemy

--- Assets/Dev/Scripts/Enemy/States/WatchState.cs
+++ Assets/Dev/Scripts/Enemy/States/WatchState.cs
@@ -6,20 +6,51 @@ public class WatchState : MonoBehaviour, IState
     [SerializeField] CinemachineCamera cam;
     [SerializeField] Animator animCinemachine;
     [SerializeField] Animator animator;
+
+    [SerializeField] EnemyDetectionBar _enemyDetectionBar;
+    [SerializeField] float _currentDetectionValue, _maxDetectionValue;
+    [SerializeField] public bool _detecting;
+    bool _lockAnimation;
+
     public void OnFinish()
     {
-       
+        _currentDetectionValue = 0; //resetear valor de detección al pasar a otro estado
     }
 
     public void OnStart()
     {
-        cam.Target = new CameraTarget { TrackingTarget = transform, LookAtTarget = transform };
+        InvokeRepeating("UpdateDetection", 0, 1f);
+
+        /*cam.Target = new CameraTarget { TrackingTarget = transform, LookAtTarget = transform };
         animCinemachine.Play("EnemyCameraFinishEvent");
-        animator.SetTrigger("Pointing");
+        animator.SetTrigger("Pointing");*/
     }
 
     public void OnUpdate()
     {
-       
+        if (!_lockAnimation && _currentDetectionValue == _maxDetectionValue) //si se acabó el valor máximo de detección, poner animación
+            DetectionAnimation();
+
+        if (!_detecting) //PENDIENTE: resolver retorno al estado de patrol, capaz todo esto deberia estar en OnFinish()
+        {
+            _enemyDetectionBar.ResetBar();
+            CancelInvoke("UpdateDetection");
+        }
+    }
+
+    private void UpdateDetection()
+    {
+        _currentDetectionValue += 1;
+        _enemyDetectionBar.DecreaseBar(_currentDetectionValue);
+        //Debug.Log(_currentDetectionValue);
+    }
+
+    private void DetectionAnimation()
+    {
+        cam.Target = new CameraTarget { TrackingTarget = transform, LookAtTarget = transform };
+        animCinemachine.Play("EnemyCameraFinishEvent");
+        animator.SetTrigger("Pointing");
+
+        _lockAnimation = true; //llamar animación una sola vez
     }
 }
--- Assets/User_EzeGuerra/Audio/Scripts/SoundEvent.cs
+++ Assets/User_EzeGuerra/Audio/Scripts/SoundEvent.cs
@@ -1,21 +1,25 @@
 using UnityEngine;
+using System.Linq;
+using UnityEditor.Animations;
 
 public class SoundEvent : MonoBehaviour
 {
     AudioSource _audioSource;
     [SerializeField] AudioClip[] _clips;
 
-    private void Awake() //automatizar referencia
+    private void Awake()
     {
         _audioSource = gameObject.GetComponent<AudioSource>();
     }
 
-    public void PlaySoundOnFrame()
+    public void CheckClip()
     {
-        int randInd = Random.Range(0, _clips.Length); //meter random dentro de la asignacion dsp
-        Debug.Log(gameObject);
-
-        _audioSource.PlayOneShot(_clips[randInd]);
+        //if(clip al que esta asignado este event == clip actual/ con mas peso dentro del blend tree)
+            //PlaySoundOnFrame()
+    }
+    void PlaySoundOnFrame() //al blendearse las animaciones, el sonido se duplica/ continúa sonando
+    {
+        _audioSource.PlayOneShot(_clips[Random.Range(0, _clips.Length)]);
     }
 
 }
